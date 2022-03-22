c840a783970b0d04bd4a59682ada4c62b60d5edf
LeandroCarolino01 <carolino.leandro@gmail.com>
2022-03-22 23:26:29 +0100

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,11 +16,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'form-test'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('form-test');
-  });
+ 
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'form-test';
+export class AppComponent implements OnInit {
+  locations: string[] = ['Downtown', 'S. Bay', 'Lakeside'];
+  volunteerForm!: FormGroup;
+  constructor(private fb: FormBuilder){ }
+
+  ngOnInit(){
+    this.initializeForm();
+  }
+
+  initializeForm(){
+    this.volunteerForm = this.fb.group({
+      name: 'Name here',
+      phoneNumber: '',
+      preferredLocation: '',
+      animals: this.fb.group({
+        dogs: false,
+        cats: false,
+        reptiles: false
+      }),
+      references: this.fb.array([this.fb.control('')])
+    })
+  }
+
+  onSubmit(): void {
+    console.log(this.volunteerForm)
+  }
+
+  addEmail():void {
+    this.references.push(this.fb.control(''));
+  }
+
+  selectedLocation(event: any): void {
+    this.volunteerForm.patchValue({
+      preferredLocation: event.target.value
+    })
+  }
+
+  removeEmail(index: number): void {
+    this.references.removeAt(index);
+  }
+
+  get references(): FormArray {
+    return this.volunteerForm.get('references') as FormArray;
+  }
 }
